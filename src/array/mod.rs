affc88f3374f6f75508e5f71311e5ea4c3bee70b
bit-aloo <sshourya17@gmail.com>
2025-10-20 20:32:24 +0530

    type gynastic part 1

---
+++ src/array/mod.rs
@@ -0,0 +1,61 @@
+//! Contains array types for the system
+//!
+//! This crate contains two category of structs -- ArrayBuilder and Array. Developers may use
+//! ArrayBuilder to create an Array. ArrayBuilder and Array are reciprocal types. We can associate
+//! an Array with an ArrayBuilder at compile time. This module also contains examples on how to use
+//! generics around the Array and ArrayBuilder.
+
+use crate::array::iterator::ArrayIterator;
+
+mod iterator;
+mod primitive_array;
+mod string_array;
+
+/// [`Array`] is a collection of data of the same type
+pub trait Array: Send + Sync + Sized + 'static {
+    /// The corresponding [`ArrayBuilder`] of this [`Array`].
+    ///
+    /// We constraint the associated type so that `Self::Builder::Array = Self`
+    type Builder: ArrayBuilder<Array = Self>;
+
+    /// The owned item of the array.
+    type OwnedItem: 'static + std::fmt::Debug;
+
+    /// Type of the item that can be retrieved from the [`Array`]. For example,
+    /// we can get a `i32` from [`Int32Array`], while [`StringArray`] produces a`&str`. As we need a lifetime
+    /// that is the same as `self` for `&str`, we use GAT here.
+    type RefItem<'a>: Copy + std::fmt::Debug;
+
+    /// Retrieve a reference to value.
+    fn get(&self, idx: usize) -> Option<Self::RefItem<'_>>;
+
+    /// Number of items of array.
+    fn len(&self) -> usize;
+
+    /// Indicates whether this array is empty
+    fn is_empty(&self) -> bool {
+        self.len() == 0
+    }
+
+    /// Get iterator of this array.
+    fn iter(&self) -> ArrayIterator<'_, Self>;
+}
+
+/// [`ArrayBuilder`] builds an [`Array`]
+pub trait ArrayBuilder {
+    /// The corresponding [`Array`] of this [`ArrayBuilder`].
+    ///
+    /// Here we use associated type to constraint the [`Array`] type of this builder, so that
+    /// `Self::Array::Builder == Self`. This property is very useful when constructing generic
+    /// functions, and may help a lot when implementing expressions.
+    type Array: Array<Builder = Self>;
+
+    /// Creates a new builder with `capacity`.
+    fn with_capacity(capacity: usize) -> Self;
+
+    /// Append a value to builder.
+    fn push(&mut self, value: Option<<Self::Array as Array>::RefItem<'_>>);
+
+    /// Finish build and return a new array.
+    fn finish(self) -> Self::Array;
+}
---
+++ src/lib.rs
@@ -0,0 +1,7 @@
+//! Type exercise in Rust
+//!
+//! This is on how to use the Rust type system to build necessary components in database system.
+//! We leverage the Rust type system to minimize runtime cost and make our development process
+//! easier.
+
+pub mod array;
